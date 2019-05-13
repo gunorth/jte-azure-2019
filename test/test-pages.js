6b8b0e85e9ddc3ce27aac96520e22f9e9bab2fdc
Guy N <gunorth@microsoft.com>
2019-05-13 23:32:57 +0200

    mise en place TU

---
+++ test/test-pages.js
@@ -0,0 +1,29 @@
+var expect  = require('chai').expect;
+var request = require('request');
+
+it('Main page content', function(done) {
+    done();
+});
+it('Main page status', function(done) {
+        done();
+});
+
+it('About page content', function(done) {
+        done();
+});
+
+it('About page status', function(done) {
+    done();
+});
+
+it('Content #1', function(done) {
+    done();
+});
+
+it('Content #2', function(done) {
+    done();
+});
+
+it('Content #3', function(done) {
+    done();
+});
